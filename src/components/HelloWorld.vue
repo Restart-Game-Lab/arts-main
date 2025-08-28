224ced991352dd73b1f3e495d72c17040aa6ec4b
preca-hoshino <001@labmem.com.cn>
2025-08-28 21:49:57 +0800

    [Ref] 重构组件结构，移除未使用的组件，更新首页内容，优化样式

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,23 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
-</script>
-
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
+  <div class="hello">
+    <h1>Hello World</h1>
   </div>
 </template>
 
+<script setup lang="ts">
+// 这里可以添加组件逻辑
+</script>
+
 <style scoped>
+.hello {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
 h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+  font-size: 3rem;
+  color: var(--color-heading);
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <HelloWorld />
   </main>
 </template>
+
+<style scoped>
+main {
+  width: 100%;
+  height: 100%;
+}
+</style>
